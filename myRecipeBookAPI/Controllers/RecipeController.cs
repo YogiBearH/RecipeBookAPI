ff62276ebbcc3cf5662e697176cb7a1e06ffc77f
John Nguyen <jnguyen@catalyte.io>
2023-04-07 14:23:41 -0600

    Start of myRecipeBook

---
+++ myRecipeBookAPI/Controllers/RecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myRecipeBookAPI.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public string Index()
+        {
+            return "This is the page for the Recipes";
+        }
+    }
+}
---
+++ myRecipeBookAPI/Data/Models/RecipeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace myRecipeBookAPI.Data.Models
+{
+    public class RecipeModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? Version { get; set; }
+
+        public int CookTime { get; set; }
+
+        public string Desciption { get; set; }
+
+    }
+}
