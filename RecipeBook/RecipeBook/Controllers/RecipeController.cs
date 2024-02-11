a38488b7fd4090b76eacbc2ca3409fbc3f504d42
John Nguyen <jnguyen@catalyte.io>
2024-02-10 23:45:22 -0700

    Added DeleteRecipeById in the Providers

---
+++ RecipeBook/RecipeBook/Controllers/RecipeController.cs
@@ -0,0 +1,16 @@
+﻿using RecipeBook.Models;
+using RecipeBook.Pro
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RecipeBook.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        public readonly ILogger<RecipeController> Logger;
+        public readonly IRecipeP
+    }
+}
